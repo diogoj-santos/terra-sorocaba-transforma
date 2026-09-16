07ddd94bf3802f7c84ebabeeb113bb307455a7e9
Diogo Santos <134185756+diogoj-santos@users.noreply.github.com>
2026-09-15 23:14:50 -0300

    Integrate Google Tag Manager into the root component
    Added Google Tag Manager script and noscript iframe for tracking.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -99,6 +99,13 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-1BZ4KWF12D');`,
       },
+      {
+        children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MF6P82FM');`,
+      },
     ],
   }),
   shellComponent: RootShell,
@@ -114,6 +121,15 @@ function RootShell({ children }: { children: ReactNode }) {
         <HeadContent />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MF6P82FM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         {children}
         <Scripts />
       </body>
